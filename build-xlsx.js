c87bfc627e59ad6fae88b6475f3b4c88bc9b82ae
chengyurou <18829292695@163.com>
2017-03-20 15:16:27 +0800

    bulid xlsx

--- build-xlsx.js
+++ build-xlsx.js
@@ -1,3 +1,12 @@
-/**
- * Created by cyr on 3/20/17.
- */
+const xlsx = require('node-xlsx');
+const fs = require('fs');
+
+const data = [
+  [1, 2, 3],
+  [true, false, null, 'sheetjs'],
+  ['foo', 'bar', Date.now(), '0.3'],
+  ['baz', null, 'qux']
+];
+
+const buffer = xlsx.build([{name: "mySheetName", data: data}]);
+fs.writeFileSync('./build.xlsx', buffer);
